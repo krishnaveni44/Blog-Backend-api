a023dc40e6bbf14a5c0f77706ee4cef8324f3570
krishnaveni44 <krishnaveni44@gmail.com>
2022-06-28 10:34:02 +0530

    1

--- index.js
+++ index.js
@@ -8,8 +8,16 @@ const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categories");
 const multer = require("multer");
 const path = require("path");
+const cors = require("cors");
 
 dotenv.config();
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname, "/images")));
 
